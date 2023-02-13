5537019cd9ba04645c2a63babafb457089c69be5
YilverQ <yilver0906@gmail.com>
2023-02-13 18:43:56 -0400

    se borró a Gustavo de la lista de empleados

--- templates/nuestro_equipo_DATA.php
+++ templates/nuestro_equipo_DATA.php
@@ -9,13 +9,13 @@
 					"Social"=> $social_Encli,
 					"Imagen"=> "encli-velasquez.webp");
 	
-	$social_Gustavo = ["Facebook"=>"https://www.facebook.com/gustavo.ponce2",
-					 "Instagram"=>"https://www.instagram.com/gustavoponce206",
-					 "Whatsapp"=>"https://walink.co/511551"];
-	$gustavo = array("Nombre"=>"Gustavo Ponce", 
-					"Cargo"=>"Gerente de ventas",
-					"Social"=> $social_Gustavo,
-					"Imagen"=> "gustavo-ponce.webp");
+	$social_Gustavo_disabled = ["Facebook"=>"https://www.facebook.com/gustavo.ponce2",
+								"Instagram"=>"https://www.instagram.com/gustavoponce206",
+								"Whatsapp"=>"https://walink.co/511551"];
+	$gustavo_disabled = array("Nombre"=>"Gustavo Ponce", 
+							"Cargo"=>"Gerente de ventas",
+							"Social"=> $social_Gustavo,
+							"Imagen"=> "gustavo-ponce.webp");
 
 	$social_Yilver = ["Facebook"=>"https://www.facebook.com/yilver0906",
 					 "Instagram"=>"https://bit.ly/3AFr1BQ",
@@ -25,19 +25,19 @@
 					"Social"=> $social_Yilver,
 					"Imagen"=> "yilver-quevedo.webp");
 
-	$social_Anabel = ["Facebook"=>"https://www.facebook.com/lopezaraque",
+	$social_Anabela = ["Facebook"=>"https://www.facebook.com/lopezaraque",
 					 "Instagram"=>"https://www.instagram.com/jelitalpz7",
 					 "Whatsapp"=>"https://wa.link/ac0hif"];
-	$anabel = array("Nombre"=>"Anabel López", 
+
+	$anabela = array("Nombre"=>"Anabela López", 
 					"Cargo"=>"Social media",
-					"Social"=> $social_Anabel,
+					"Social"=> $social_Anabela,
 					"Imagen"=> "anabel-lopez.webp");
 
 	//Array
 	$meTeam[0] = $encli;
-	$meTeam[1] = $gustavo;
-	$meTeam[2] = $yilver;
-	$meTeam[3] = $anabel;
+	$meTeam[1] = $yilver;
+	$meTeam[2] = $anabela;
 
 	//Imprimir todos los datos.
 	//print_r($meTeam);
